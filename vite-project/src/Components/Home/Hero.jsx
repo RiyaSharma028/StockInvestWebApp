a7cf4d503c0624bd0ef0e5289f04cec218117e3c
Riya Sharma <riyasharmaa00028@gmail.com>
2025-05-07 12:26:07 +0530

    Made the Hero Section

--- vite-project/src/Components/Home/Hero.jsx
+++ vite-project/src/Components/Home/Hero.jsx
@@ -3,15 +3,14 @@ import homeHero from "../../../public/Media/homeHero.png"
 
 function Hero() {
   return (
-    <div className='container'>
+    <div className="container ">
        <div className="row justify-content-center text-center">
-      <img src={homeHero} alt="Boooo" className="w-75 img-fluid mb-5 " />
-      <h1 className=''>Invest in everything</h1>
-      <p className='mb-5'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-      <button className="btn btn-primary  w-25 fw-bold">SignUp Now</button>
-  </div>
+         <img src={homeHero} alt="Hero Image" className='w-75 mb-5' />
+         <h1>Invest in everything</h1>
+         <p className='mb-3'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+         <button className='btn btn-primary w-25 '>SignUp For Free</button>
+       </div>
     </div>
-  
   )
 }
 
