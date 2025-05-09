1fe7f5304f7af7e142cae77e571a21cc5f71071b
Riya Sharma <riyasharmaa00028@gmail.com>
2025-05-09 14:26:15 +0530

    Created Education Component and OpenAccount Component made some changes in Hero component

--- vite-project/src/Components/Home/Hero.jsx
+++ vite-project/src/Components/Home/Hero.jsx
@@ -7,8 +7,8 @@ function Hero() {
        <div className="row justify-content-center text-center">
          <img src={homeHero} alt="Hero-Image" className='w-75 mb-5' />
          <h1>Invest in everything</h1>
-         <p className='mb-3'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
-         <button className='btn btn-primary p-2 fw-bold mb-5' style={{width : "15%"}}>SignUp For Free</button>
+         <p className='mt-3'>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+         <button className='btn btn-primary p-2 fw-bold mb-5 mt-3' style={{width : "15%"}}>SignUp For Free</button>
        </div>
     </div>
   )
