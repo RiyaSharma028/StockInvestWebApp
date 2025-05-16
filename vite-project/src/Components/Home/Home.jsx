75190342ae2f0a85dc42b954a756c8703a1e1956
Riya Sharma <riyasharmaa00028@gmail.com>
2025-05-16 13:14:33 +0530

    Added Routes

--- vite-project/src/Components/Home/Home.jsx
+++ vite-project/src/Components/Home/Home.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import Navbar from '../../Navbar'
 import Hero from './Hero'
 import OpenAccount from '../../OpenAccount'
-import Footer from '../../Footer'
 import Trust from './trust'
 import Pricing from './pricing'
 import Education from './Education'
@@ -10,13 +8,12 @@ import Education from './Education'
 function Home() {
   return (
     <>
-     <Navbar></Navbar>
      <Hero></Hero>
      <Trust></Trust>
      <Pricing></Pricing>
      <Education></Education>
      <OpenAccount></OpenAccount>
-     <Footer></Footer>
+    
     </>
   )
 }
