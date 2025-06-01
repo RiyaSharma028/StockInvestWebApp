8eb6d30b9c090f06bd2f167a5dc088396267a37a
RiyaSharma028 <riyasharmaa00028@gmail.com>
2025-06-01 13:14:16 +0530

    Completed product-component

--- Frontend/vite-project/src/Components/Pricing/Brokerage.jsx
+++ Frontend/vite-project/src/Components/Pricing/Brokerage.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
-function Brokerage(props) {
+function Brokerage() {
     return (
-        <div>
-            
+        <div className='container'>
+            <div className="row p-5 border-top mt-5">
+                 <div className="col-8 p-4">
+                    <a href="#" style={{textDecoration :"none"}}><h3 className='fs-5'>Brokerage calculator</h3></a>
+                    <ul>
+                        <li>Calculates total brokerage and charges instantly.</li>
+                        <li>Supports equity, F&O, currency, and commodity trades.</li>
+                        <li>Shows net profit or loss after all charges.</li>
+                        <li>Simple and easy-to-use interface.</li>
+                        <li>Helps in planning trades with cost transparency.</li>
+                    </ul>
+                 </div>
+                 <div className="col-4 p-4">
+                   <a href="#" style={{textDecoration :"none"}}><h3 className='fs-5'>list of charges</h3></a>
+                 </div>
+            </div>
+           
         </div>
     );
 }
